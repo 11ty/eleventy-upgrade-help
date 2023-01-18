8ff0c251b11898c8cb7e6a09abc17ebdd03b9515
Zach Leatherman <zachleatherman@gmail.com>
2023-01-18 13:46:01 -0600

    Fixes #5

--- src/render-template-no-arg.js
+++ src/render-template-no-arg.js
@@ -14,7 +14,7 @@ module.exports = function(eleventyConfig) {
 	}
 
 	// Handle overrides to the renderTemplate tag name
-	let tagName = renderishPlugins[0].options.tagName || "renderTemplate";
+	let tagName = renderishPlugins[0].options?.tagName || "renderTemplate";
 	if(tagName in eleventyConfig.javascriptFunctions || tagName in eleventyConfig.nunjucksTags || tagName in eleventyConfig.liquidTags) {
 		console.log(chalk.blue(`[${pkg.name}]`), chalk.blue("NOTICE"), `This project is using the Render plugin and the \`${tagName}\` shortcode behavior changed slightly. In Eleventy 2.0 when you call \`${tagName}\` without passing in a syntax, it will use the parent template syntax (instead of plain text). Make sure you review any \`${tagName}\` usage. ${readMore}`);
 	}
