f34a6bb5b29f9ebe9b9aa99bf7a6737bb0658903
Zach Leatherman <zachleat@users.noreply.github.com>
2024-07-24 11:50:39 -0500

    Remove deprecated checks

--- src/deprecated-checks/indented-code-blocks.js
+++
@@ -1,6 +0,0 @@
-const chalk = require("kleur");
-const pkg = require("../package.json");
-
-module.exports = function(eleventyConfig) {
-  console.log(chalk.blue(`[${pkg.name}]`), `${chalk.blue("NOTICE")} Markdown’s indented code blocks have been disabled by default in 2.0. Unfortunately, this plugin does *NOT* currently test whether this change affects your content. You can re-enable this feature using \`eleventyConfig.amendLibrary("md", mdLib => mdLib.enable("code"))\`. Read more: https://www.11ty.dev/docs/languages/markdown/#indented-code-blocks`);
-};
--- src/deprecated-checks/passthrough-all.js
+++
@@ -1,7 +0,0 @@
-const chalk = require("kleur");
-const pkg = require("../package.json");
-
-module.exports = function(eleventyConfig) {
-	// This throws an unknown command line flag error in Eleventy core, so if we’ve made it here—we’ve passed the test.
-  console.log(chalk.green(`[${pkg.name}]`), chalk.green("PASSED"), `This project is not using the \`--passthroughall\` command line flag. Read more at https://www.11ty.dev/docs/copy/#passthrough-everything`);
-};
